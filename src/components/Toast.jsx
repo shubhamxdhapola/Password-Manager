661f1f4e4fcad25f8001ddba4b964b89957f56ff
Shubham Dhapola <shubhamdhapola143@gmail.com>
2025-02-17 08:29:34 +0530

    Reduced in toast time

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -4,7 +4,7 @@ export default function Toast() {
     return (
         <ToastContainer
         position="top-right"
-        autoClose={5000}
+        autoClose={2000}
         hideProgressBar={false}
         newestOnTop
         closeOnClick
